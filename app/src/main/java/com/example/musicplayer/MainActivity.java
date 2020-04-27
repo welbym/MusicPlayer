e961f776c6362ad0cb789b538947860ebfb77e36
welbym <welbymoore0816@gmail.com>
2020-04-26 23:43:58 -0500

    Added text that shows current song being played

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -28,6 +28,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -54,6 +55,8 @@ public class MainActivity extends AppCompatActivity implements SongAdapter.OnSon
 
     public MediaPlayer player;
     private ArrayList<Song> songList;
+
+    private TextView nowPlayingText;
 
     private MediaService mediaService;
     private Intent playIntent;
@@ -87,6 +90,7 @@ public class MainActivity extends AppCompatActivity implements SongAdapter.OnSon
 
             setSongList();
             setFragments();
+            nowPlayingText = findViewById(R.id.now_playing_text);
             findViewById(R.id.play_pause_button).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -97,6 +101,7 @@ public class MainActivity extends AppCompatActivity implements SongAdapter.OnSon
                 @Override
                 public void onClick(View v) {
                     mediaService.stopSong();
+                    nowPlayingText.setText("");
                 }
             });
         }
@@ -196,6 +201,7 @@ public class MainActivity extends AppCompatActivity implements SongAdapter.OnSon
     public void onSongClick(int position, LinearLayout linearLayout) {
         mediaService.setSong(position);
         mediaService.playSong();
+        nowPlayingText.setText(songList.get(position).getTitle());
     }
 
     private ServiceConnection musicConnection = new ServiceConnection() {
